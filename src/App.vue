0c8db22e2d2d298d99e823c505068d638020547b
菜瓜 <42505233+WCaiGua@users.noreply.github.com>
2021-09-27 21:05:04 +0800

    Initialize fix-use-pag

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/form">Form Validation</router-link> |
+      <router-link to="/posts">Paginated Posts</router-link> |
+      <router-link to="/images">Infinite Images</router-link> |
+      <router-link to="/upload">Upload Dropzone</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding-bottom: 10px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Posts.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="flex flex-col items-center text-left">
+      <article v-for="(post, i) in posts" :key="i">
+        <Post :post="post" />
+      </article>
+    </div>
+    <div v-if="posts.length" class="pagination-box">
+      <Pagination
+        :pagination="pagination"
+        @set="pagination.set($event)"
+        @prev="pagination.prev()"
+        @next="pagination.next()"
+        @last="pagination.last()"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, watch, createComponent } from '@vue/composition-api'
+import usePagination from '../composables/use-pagination'
+import * as api from '../api'
+
+import Post from '@/components/Post'
+import Pagination from '@/components/Pagination.vue'
+export default createComponent({
+  name: 'ShowPosts',
+  components: {
+    Post,
+    Pagination,
+  },
+  setup() {
+    const pagination = usePagination()
+
+    const posts = ref([])
+    const allPosts = ref([])
+
+    function setCurrentPosts() {
+      posts.value = allPosts.value.slice(
+        pagination.offset.value,
+        pagination.offset.value + pagination.perPage.value
+      )
+    }
+    api.posts
+      .get({
+        limit: 100,
+      })
+      .then(result => {
+        allPosts.value = result
+        pagination.total.value = result.length
+        setCurrentPosts()
+      })
+
+    // load more posts when page has been increased
+    watch(pagination.currentPage, setCurrentPosts)
+
+    return {
+      allPosts,
+      posts,
+      pagination,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.pagination-box {
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
